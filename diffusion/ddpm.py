d3b68bfc5cef0f2aee1d34813e1041c88005856d
DilumFernando <90143541+DilumFernando@users.noreply.github.com>
2023-12-01 02:24:00 +0530

    lamb = 100

--- diffusion/ddpm.py
+++ diffusion/ddpm.py
@@ -238,7 +238,7 @@ class DDPM(pl.LightningModule):
         relu_regularizer = nn.ReLU()        
         
         # compute loss
-        lamb = 10
+        lamb = 100
         loss = self.criterion(eps_pred, eps) + lamb*np.maximum(0, iso_difference)
         return loss
 
