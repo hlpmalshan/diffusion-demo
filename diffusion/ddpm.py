55db62f9037122a4355b229721f2fb9d0b5877e6
DilumFernando <90143541+DilumFernando@users.noreply.github.com>
2023-12-02 19:35:11 +0530

    steps = 500

--- diffusion/ddpm.py
+++ diffusion/ddpm.py
@@ -211,10 +211,10 @@ class DDPM(pl.LightningModule):
         '''Generate random samples through the reverse process.'''
         x_denoised = torch.randn(num_samples, *sample_shape, device=self.device) # Lightning modules have a device attribute
         isotropy = []
-        for tidx in reversed(range(250)):
+        for tidx in reversed(range(500)):
             # generate random sample
             if tidx > 0:
-                x_denoised = self.denoise_step(x_denoised, 4*tidx, random_sample=True)
+                x_denoised = self.denoise_step(x_denoised, 2*tidx, random_sample=True)
                 iso = self.isotropy(x_denoised)
                 isotropy.append(iso)
             # take the mean in the last step
