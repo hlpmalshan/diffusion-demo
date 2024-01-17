1644f5c12e2d2c41f3a83fbb1ba10196c577d155
DilumFernando <90143541+DilumFernando@users.noreply.github.com>
2024-01-17 13:18:37 +0530

    Update ddpm.py

--- diffusion/ddpm.py
+++ diffusion/ddpm.py
@@ -251,10 +251,10 @@ class DDPM(pl.LightningModule):
         # compute loss
         
         squared_norm_preds = torch.mean(torch.sum(eps_pred**2, dim=1))
-        dim_ = torch.tensor(2.0, requires_grad=True)
+        # dim_ = torch.tensor(2.0, requires_grad=True)
         # target_dim = torch.one_like(squared_norm_preds, requires_grad=True)*dim_2
         
-        norm_loss = (squared_norm_preds - dim_)**2
+        norm_loss = (squared_norm_preds)
         simple_diff_loss = self.criterion(eps_pred, eps)
         
         loss = simple_diff_loss + reg*norm_loss
