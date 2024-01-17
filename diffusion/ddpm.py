5b5c7fa1528fc6af504ae3f6a02eacdec57982a2
DilumFernando <90143541+DilumFernando@users.noreply.github.com>
2024-01-17 22:17:30 +0530

    Update ddpm.py

--- diffusion/ddpm.py
+++ diffusion/ddpm.py
@@ -262,7 +262,7 @@ class DDPM(pl.LightningModule):
         self.eps_pred_list.append(eps_pred)
 
         # Regularizer hyperparameter
-        reg = 0.1
+        reg = 0.05
         kurt_reg = 0.01
         
         # compute squared norm loss
