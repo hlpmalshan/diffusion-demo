ef2d868e1315e40f61a6232b23f6cfabfc722ab2
DilumFernando <90143541+DilumFernando@users.noreply.github.com>
2024-01-17 20:54:54 +0530

    Update ddpm.py

--- diffusion/ddpm.py
+++ diffusion/ddpm.py
@@ -231,6 +231,18 @@ class DDPM(pl.LightningModule):
         data = data.detach().cpu().numpy()
         iso = np.vdot(data, data) / len(data)
         return iso
+
+    def kurtosis(self, data):
+        means = torch.mean(vectors, dim=1, keepdim=True)
+        centered_vectors = vectors - means
+        fourth_moments = torch.mean(centered_vectors**4, dim=2)
+        variances = torch.mean(centered_vectors**2, dim=2)
+        
+        # Handling the case where variance is zero
+        variances[variances == 0] = 0.001
+        
+        kurtoses = fourth_moments / (variances**2) - 3
+        return kurtoses
     
     def loss(self, x):
         '''Compute stochastic loss.'''
@@ -248,17 +260,20 @@ class DDPM(pl.LightningModule):
         self.eps_pred_list.append(eps_pred)
 
         # Regularizer hyperparameter
-        reg = 0.15
+        reg = 0.2
+        kurt_reg = 0.1
         
-        # compute loss
-        
+        # compute squared norm loss
         squared_norm_preds = torch.mean(torch.sum(eps_pred**2, dim=2))
         dim_ = torch.tensor(2.0, requires_grad=True)
+
+        # compute kurtosis loss
+        kurtosis_loss = torch.mean(kurtosis(eps_pred))**2
         
         norm_loss = self.criterion(squared_norm_preds, dim_)
         simple_diff_loss = self.criterion(eps_pred, eps)
         
-        loss = simple_diff_loss + reg*norm_loss
+        loss = simple_diff_loss + reg*norm_loss + kurt_reg*kurtosis_loss
 
 
         return loss, simple_diff_loss, norm_loss
