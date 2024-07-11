3f2277564fa80f23717914ac6adafccf398c20e1
Shakthi Perera <59946892+ShakthiPerera@users.noreply.github.com>
2024-07-11 19:39:04 +0530

    Update ddpm_iso_ratio.py

--- diffusion/ddpm_iso_ratio.py
+++ diffusion/ddpm_iso_ratio.py
@@ -236,10 +236,10 @@ class DDPM(pl.LightningModule):
         # frob_norm = torch.mean(torch.sum(eps_pred**2, dim=len(eps_pred.shape)-1))/len(x.shape[1:])
         covariance_matrix = eps_pred.T@eps_pred/eps_pred.shape[0]
 
-        lambdas = torch.sort(torch.linalg.eigvals(covariance_matrix), descending=True)
+        lambdas = torch.sort(torch.linalg.eigvals(covariance_matrix).cpu(), descending=True)
         iso_ratio = lambdas[0]/lambdas[1]
 
-        norm_loss = torch.criterion(iso_ratio, torch.tensor(1.0, dtype=eps_pred.dtype).to(eps_pred.device))
+        norm_loss = torch.criterion(iso_ratio.to(eps_pred.device), torch.tensor(1.0, dtype=eps_pred.dtype).to(eps_pred.device))
 
         simple_diff_loss = self.criterion(eps_pred, eps)
 
